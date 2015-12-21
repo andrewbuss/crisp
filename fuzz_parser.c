edcfd9f1811859de6bb1c4a299dea429857b1d7e
Andrew Buss <abuss@ucsd.edu>
2015-12-21 03:59:05 -0800

    Explain fuzz_parser

--- fuzz_parser.c
+++ fuzz_parser.c
@@ -1,5 +1,11 @@
 #include "crisp.h"
 
+// This shell program reads a line from stdin, parses it,
+// prints it out again, parses it once more, prints it again,
+// then checks whether the two outputs of print are identical
+// If they are not, the parse and print functions are not stable
+// The program crashes to signal this so a fuzzer can generate and
+// minimize a test case for an error
 int main(int argc, char** argv) {
     char* line = NULL;
     size_t len = 0;
@@ -8,14 +14,12 @@ int main(int argc, char** argv) {
 
     char* to_parse = line;
     cell expr = parse(&to_parse);
-    char* formatted_1 = strdup(print_cell(expr));
+    char* formatted_1 = print_cell(expr);
     to_parse = formatted_1;
     expr = parse(&to_parse);
     char* formatted_2 = print_cell(expr);
 
     printf("%s\n%s\n", formatted_1, formatted_2);
 
-    if (strcmp(formatted_1, formatted_2)) {
-        *(int*) 0 = 0;
-    }
+    if (strcmp(formatted_1, formatted_2)) *(int*) 0 = 0;
 }
